da9012ed7b970e4cf360f3281088c469339bae28
Rachelle Scheijen <rachelle.scheijen@tenzinger.com>
2024-12-15 10:00:33 +0100

    Added Symfony 7.2 compatibility

--- rector.php
+++ rector.php
@@ -11,7 +11,7 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     ->withSets([
-        LevelSetList::UP_TO_PHP_82,
+        LevelSetList::UP_TO_PHP_83,
     ])
     ->withImportNames(
         importShortClasses: false,
